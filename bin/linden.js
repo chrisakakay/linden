f2fe6a75d74623d8ec903fc703e491ed9093151a
Krisztian Nagy <chrisakakay@gmail.com>
2016-05-23 23:44:29 +0200

    removed unneccessary code

--- bin/linden.js
+++ bin/linden.js
@@ -8,13 +8,6 @@ var g           = global.LINDEN;
 var conf        = require('../lib/configurator');
 var helper      = require('../lib/helper');
 
-// Handling failed exit
-process.once('exit', function(code) {
-    if (code === 0 && g.failed) {
-        process.exit(1);
-    }
-});
-
 init();
 
 function init() {
