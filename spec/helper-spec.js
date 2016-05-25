cd241797c73383ba54ba57b4e76b529482a33e1d
Krisztian Nagy <Krisztian_Nagy@epam.com>
2016-05-25 11:28:42 +0200

    config init added

--- spec/helper-spec.js
+++ spec/helper-spec.js
@@ -35,6 +35,30 @@ describe('Helper', function() {
         });
     });
 
+    describe('isArray()', function() {
+        it('should handle undefined', function() {
+            expect(helper.isArray()).toEqual(false);
+            expect(helper.isArray(undefined)).toEqual(false);
+        });
+
+        it('should handle empty array', function() {
+            expect(helper.isArray([])).toEqual(false);
+        });
+
+        it('should handle strings', function() {
+            expect(helper.isArray('')).toEqual(false);
+            expect(helper.isArray('a')).toEqual(false);
+        });
+
+        it('should handle null', function() {
+            expect(helper.isArray(null)).toEqual(false);
+        });
+
+        it('should return true', function() {
+            expect(helper.isArray(['a'])).toEqual(true);
+        });
+    });
+
     describe('fileExists()', function() {
         it('should handle existing file', function() {
             expect(helper.fileExists('./spec/helper-spec.js')).toEqual(true);
