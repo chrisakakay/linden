821ff2595ae7b8610491baf3d4bd93848bcefbda
Krisztian Nagy <chrisakakay@gmail.com>
2016-06-22 19:30:16 +0200

    exclude in reporting

--- bin/linden.js
+++ bin/linden.js
@@ -9,30 +9,26 @@ var conf        = require('../lib/configurator');
 var helper      = require('../lib/helper');
 var runner      = require('../lib/runner');
 
-init();
-
-function init() {
-    if (g.tasks.length === 0) {
-        if (g.flagVersion) {
-            helper.printVersion();
-            process.exit(0);
-        }
-
-        if (g.flagHelp) {
-            helper.printHelp();
-            process.exit(0);
-        }
-
-        conf.open(g.configFile);
-    } else {
-        switch (g.tasks[0]) {
-            case 'init':
-                conf.init();
-                break;
-            case 'run':
-                conf.open(g.configFile);
-                break;
-            default: g.log('Unknown command');
-        }
+if (g.tasks.length === 0) {
+    if (g.flagVersion) {
+        helper.printVersion();
+        process.exit(0);
     }
+
+    if (g.flagHelp) {
+        helper.printHelp();
+        process.exit(0);
+    }
+
+    g.tasks.push('run');
+}
+
+switch (g.tasks[0]) {
+    case 'init':
+        conf.init();
+        break;
+    case 'run':
+        conf.open(g.configFile);
+        break;
+    default: g.log('Unknown command');
 }
