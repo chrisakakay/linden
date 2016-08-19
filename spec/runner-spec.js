0305d6713ade0686895ca5c0dcb87d90d882625a
Krisztian Nagy <Krisztian_Nagy@epam.com>
2016-08-19 15:42:00 +0200

    more testing

--- spec/runner-spec.js
+++ spec/runner-spec.js
@@ -3,12 +3,21 @@
 describe('Runner', function () {
     var g           = require('../lib/globals')({}, { cwd: 'cwd', basePath: 'basePath' });
     var runner      = require('../lib/runner')(g);
+    var mockCase    = {
+        name:       'google',
+        url:        'http://google.com',
+        viewport:   {
+            width: 1920,
+            height: 1200
+        }
+    };
 
     it('should have functions', function () {
         expect(runner.init).toBeDefined();
         expect(runner.start).toBeDefined();
         expect(runner.reset).toBeDefined();
         expect(runner.getCases).toBeDefined();
+        expect(runner.getDriver).toBeDefined();
     });
 
     describe('init()', function() {
@@ -17,7 +26,7 @@ describe('Runner', function () {
 
             spyOn(g, 'log').and.callFake(function () {});
 
-            var driver = runner.init();
+            var driver = runner.init().getDriver();
 
             expect(g.log).toHaveBeenCalledWith('Initializing driver');
             expect(driver).not.toEqual(null);
@@ -27,11 +36,56 @@ describe('Runner', function () {
     });
 
     describe('start()', function() {
+        it('should do nothing without cases', function() {
+            var g = require('../lib/globals')({}, { cwd: 'cwd', basePath: 'basePath' });
 
+            spyOn(g, 'log').and.callFake(function () {});
+
+            runner.reset();
+            runner.start();
+
+            expect(g.log).toHaveBeenCalledWith('Nothing to run');
+        });
+
+        it('should do nothing with empty cases', function() {
+            var g = require('../lib/globals')({}, { cwd: 'cwd', basePath: 'basePath' });
+
+            spyOn(g, 'log').and.callFake(function () {});
+
+            runner.reset();
+            runner.init();
+            runner.start();
+
+            expect(g.log).toHaveBeenCalledWith('Driver already initialized');
+        });
+
+        it('should start if it has any case', function() {
+            var g = require('../lib/globals')({}, { cwd: 'cwd', basePath: 'basePath' });
+
+            spyOn(g, 'log').and.callFake(function () {});
+
+            runner.reset();
+            runner.start([ mockCase ]);
+
+            expect(g.log).toHaveBeenCalledWith('Initializing driver');
+            // expect(g.log).toHaveBeenCalledWith('Cases to run: 1');
+        });
     });
 
     describe('reset()', function() {
+        it('should behave...', function() {
+            runner.init();
+            runner.reset();
 
+            expect(runner.getDriver()).toEqual(null);
+        });
+
+        it('should behave...', function() {
+            runner.reset();
+            runner.reset();
+
+            expect(runner.getDriver()).toEqual(null);
+        });
     });
 
     describe('getCases()', function() {
